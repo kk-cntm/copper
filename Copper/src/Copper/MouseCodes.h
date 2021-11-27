bbec1e2d8f4fef508a3f1bbeea18e51ddb4824cc
Kirill Kogut <kirilrempov@gmail.com>
2021-11-27 10:03:30 +0200

    Added own keycodes

---
+++ Copper/src/Copper/MouseCodes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define CPR_MOUSE_BUTTON_1         0
+#define CPR_MOUSE_BUTTON_2         1
+#define CPR_MOUSE_BUTTON_3         2
+#define CPR_MOUSE_BUTTON_4         3
+#define CPR_MOUSE_BUTTON_5         4
+#define CPR_MOUSE_BUTTON_6         5
+#define CPR_MOUSE_BUTTON_7         6
+#define CPR_MOUSE_BUTTON_8         7
+#define CPR_MOUSE_BUTTON_LAST      CPR_MOUSE_BUTTON_8
+#define CPR_MOUSE_BUTTON_LEFT      CPR_MOUSE_BUTTON_1
+#define CPR_MOUSE_BUTTON_RIGHT     CPR_MOUSE_BUTTON_2
+#define CPR_MOUSE_BUTTON_MIDDLE    CPR_MOUSE_BUTTON_3
