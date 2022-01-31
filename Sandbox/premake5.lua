5577e0974b382e3ac0cc6d5ac956a717c24ad4fb
Kirill Kogut <kirilrempov@gmail.com>
2022-01-31 14:44:25 +0200

    Refactored premake files

---
+++ Sandbox/premake5.lua
@@ -0,0 +1,62 @@
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "Off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{wks.location}/Copper/src",
+        "%{wks.location}/Copper/vendor",
+        "%{wks.location}/Copper/vendor/spdlog/include",
+        "%{IncludeDir.Glad}",
+        "%{IncludeDir.glm}"
+    }
+
+    links
+    {
+        "Copper"
+    }
+
+    filter "system:macosx"
+        systemversion "latest"
+
+        xcodebuildsettings
+        {
+            ["ALWAYS_SEARCH_USER_PATHS"] = "YES"
+        }
+
+        postbuildcommands
+        {
+            -- copy assets dir to build directory
+            "cp -rf %{prj.location}/assets %{cfg.buildtarget.directory}"
+        }
+
+    filter "configurations:Debug"
+        defines
+        {
+            "CPR_DEBUG",
+            "CPR_ENABLE_ASSERTS"
+        }
+        symbols "On"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        defines "CPR_RELEASE"
+        optimize "On"
+        runtime "Release"
+
+    filter "configurations:Dist"
+        defines "CPR_DIST"
+        optimize "On"
+        runtime "Release"
